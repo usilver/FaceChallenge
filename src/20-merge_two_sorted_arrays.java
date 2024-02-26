ddcbdfdf7e992c88921b3280931300b544a9ccb7
Kundan <kumarkundan0809@icloud.com>
2024-02-26 20:27:44 +0530

    easy solution added for merging two sorted arrays

--- src/20-merge_two_sorted_arrays.java
+++ src/20-merge_two_sorted_arrays.java
@@ -6,6 +6,11 @@ class MergeTwoSortedArrays {
         int[] mergedArr = merge(arr1, arr2);
         System.out.println("Your merged array is : ");
         ArrayUtility.displayArray(mergedArr);
+        System.out.println();
+
+        // another easy solution for merging two sorted arrays
+        int[] mergedNewArray = mergeSortedArrays(arr1, arr2);
+        ArrayUtility.displayArray(mergedNewArray);
     }
 
     public static int[] merge(int[] arr1, int[] arr2){
@@ -26,5 +31,30 @@ class MergeTwoSortedArrays {
             }
         }
         return mergedArr;
+    }
+
+    // another logic to merge sorted arrays: ChatGPT
+    public static int[] mergeSortedArrays(int[] array1, int[] array2){
+        int i = 0, j = 0, k = 0;
+        int newArraySize = array1.length + array2.length;
+        int[] newArray = new int[newArraySize];
+        // ex. int[] array1 = {1, 2, 3}; int[] array2 = {4, 5, 6}
+        // ex. int[] array1 = {11, 12, 13, 14, 15}; int[] array2 = {4, 5, 6}
+        while (i < array1.length && j < array2.length){
+            if (array1[i] < array2[j]){
+                newArray[k++] = array1[i++];
+            } else {
+                newArray[k++] = array2[j++];
+            }
+        }
+        // add remaining elements from array1
+        while (i < array1.length){
+            newArray[k++] = array1[i++];
+        }
+        // add remaining elements from array2
+        while(j < array2.length){
+            newArray[k++] = array2[j++];
+        }
+        return newArray;
     }
 }
